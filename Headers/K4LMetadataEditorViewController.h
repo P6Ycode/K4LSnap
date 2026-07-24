289a7f374efd88542f4130aedfa13548ce2f8b0f
P6Ycode <thalightskintiller@gmail.com>
2026-07-24 12:24:11 -0400

    Add vault metadata editor interface

---
+++ Headers/K4LMetadataEditorViewController.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+@class K4LVaultItem;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface K4LMetadataEditorViewController : UIViewController
+- (instancetype)initWithItem:(K4LVaultItem *)item;
+@end
+
+NS_ASSUME_NONNULL_END
