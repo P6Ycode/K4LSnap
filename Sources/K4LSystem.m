7e4ac200639dd7cfd328d56d36ac4916977ed65c
P6Ycode <thalightskintiller@gmail.com>
2026-07-24 12:16:29 -0400

    Create draft and thumbnail directories

--- Sources/K4LSystem.m
+++ Sources/K4LSystem.m
@@ -12,8 +12,11 @@ NSString *K4LRootDirectory(void) {
 }
 
 NSString *K4LMediaDirectory(void) { return [K4LRootDirectory() stringByAppendingPathComponent:@"Media"]; }
+NSString *K4LThumbnailDirectory(void) { return [K4LRootDirectory() stringByAppendingPathComponent:@"Thumbnails"]; }
 NSString *K4LTemporaryDirectory(void) { return [K4LRootDirectory() stringByAppendingPathComponent:@"Temp"]; }
+NSString *K4LDraftDirectory(void) { return [K4LRootDirectory() stringByAppendingPathComponent:@"Drafts"]; }
 NSString *K4LDatabasePath(void) { return [K4LRootDirectory() stringByAppendingPathComponent:@"vault.sqlite3"]; }
+NSString *K4LPendingSendPath(void) { return [K4LRootDirectory() stringByAppendingPathComponent:@"pending-send.plist"]; }
 NSString *K4LPreferencesPath(void) {
     return [@"/var/mobile/Library/Preferences" stringByAppendingPathComponent:[K4LPreferenceDomain stringByAppendingString:@".plist"]];
 }
@@ -21,7 +24,14 @@ NSString *K4LPreferencesPath(void) {
 BOOL K4LEnsureSystemDirectories(NSError **error) {
     NSFileManager *fm = NSFileManager.defaultManager;
     NSDictionary *attrs = @{NSFilePosixPermissions: @0755};
-    for (NSString *path in @[K4LRootDirectory(), K4LMediaDirectory(), K4LTemporaryDirectory()]) {
+    NSArray<NSString *> *paths = @[
+        K4LRootDirectory(),
+        K4LMediaDirectory(),
+        K4LThumbnailDirectory(),
+        K4LTemporaryDirectory(),
+        K4LDraftDirectory()
+    ];
+    for (NSString *path in paths) {
         if (![fm createDirectoryAtPath:path withIntermediateDirectories:YES attributes:attrs error:error]) return NO;
     }
     return YES;
