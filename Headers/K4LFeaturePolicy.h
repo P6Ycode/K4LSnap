1aaa83b2f5cbea37fd08ba786cefb9ff117af99a
P6Ycode <thalightskintiller@gmail.com>
2026-07-24 12:01:15 -0400

    Remove unused feature policy switches

--- Headers/K4LFeaturePolicy.h
+++ Headers/K4LFeaturePolicy.h
@@ -2,24 +2,10 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef NS_ENUM(NSUInteger, K4LContentKind) {
-    K4LContentKindSnap,
-    K4LContentKindChat,
-    K4LContentKindRemix,
-};
-
 @interface K4LFeaturePolicy : NSObject
 + (instancetype)sharedPolicy;
 - (BOOL)galleryUploadEnabled;
-- (BOOL)screenshotSuppressionEnabled;
-- (BOOL)shouldSuppressSaveForKind:(K4LContentKind)kind;
-- (BOOL)shouldDisableTapToSaveForKind:(K4LContentKind)kind;
-- (BOOL)shouldGateSnapshotWrite;
-- (BOOL)shouldDeferSnapshotWrite;
-- (BOOL)shouldSuppressDiskWrite;
-- (BOOL)shouldKeepDeletedContent;
-- (BOOL)shouldSuppressReplayAcknowledgement;
-- (BOOL)ghostModeEnabledForKind:(K4LContentKind)kind;
+- (BOOL)launcherEnabled;
 @end
 
 NS_ASSUME_NONNULL_END
