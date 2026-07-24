d269e2e10c7086898aff759f28bea426fc29a56a
P6Ycode <thalightskintiller@gmail.com>
2026-07-24 12:02:41 -0400

    Fix rootless user-data paths

--- Sources/K4LSystem.m
+++ Sources/K4LSystem.m
@@ -7,21 +7,15 @@ NSString * const K4LNotifyVaultChanged = @"com.p6ycode.k4lsnap/vault-changed";
 NSString * const K4LNotifyDaemonPing = @"com.p6ycode.k4lsnap/daemon-ping";
 NSString * const K4LNotifyDaemonPong = @"com.p6ycode.k4lsnap/daemon-pong";
 
-static NSString *K4LBasePrefix(void) {
-    NSFileManager *fm = NSFileManager.defaultManager;
-    if ([fm fileExistsAtPath:@"/var/jb"]) return @"/var/jb";
-    return @"";
-}
-
 NSString *K4LRootDirectory(void) {
-    return [[K4LBasePrefix() stringByAppendingString:@"/var/mobile/Library/Application Support"] stringByAppendingPathComponent:@"K4LSnap"];
+    return [@"/var/mobile/Library/Application Support" stringByAppendingPathComponent:@"K4LSnap"];
 }
 
 NSString *K4LMediaDirectory(void) { return [K4LRootDirectory() stringByAppendingPathComponent:@"Media"]; }
 NSString *K4LTemporaryDirectory(void) { return [K4LRootDirectory() stringByAppendingPathComponent:@"Temp"]; }
 NSString *K4LDatabasePath(void) { return [K4LRootDirectory() stringByAppendingPathComponent:@"vault.sqlite3"]; }
 NSString *K4LPreferencesPath(void) {
-    return [[K4LBasePrefix() stringByAppendingString:@"/var/mobile/Library/Preferences"] stringByAppendingPathComponent:[K4LPreferenceDomain stringByAppendingString:@".plist"]];
+    return [@"/var/mobile/Library/Preferences" stringByAppendingPathComponent:[K4LPreferenceDomain stringByAppendingString:@".plist"]];
 }
 
 BOOL K4LEnsureSystemDirectories(NSError **error) {
