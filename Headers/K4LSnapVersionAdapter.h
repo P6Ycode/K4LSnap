f8d579a0c0173b6f3f6809094ada89251aacb5f6
P6Ycode <thalightskintiller@gmail.com>
2026-07-24 12:02:52 -0400

    Keep version adapter limited to host diagnostics

--- Headers/K4LSnapVersionAdapter.h
+++ Headers/K4LSnapVersionAdapter.h
@@ -1,17 +1,12 @@
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface K4LSnapVersionAdapter : NSObject
 + (instancetype)sharedAdapter;
 @property (nonatomic, readonly) NSString *snapchatVersion;
+@property (nonatomic, readonly) NSString *bundleIdentifier;
 - (BOOL)isSupportedVersion;
-- (void)installPrivateHooks;
-- (BOOL)sendImportedMediaAtURL:(NSURL *)url
-                      caption:(nullable NSString *)caption
-                   wholeStory:(BOOL)wholeStory
-                        error:(NSError **)error;
 @end
 
 NS_ASSUME_NONNULL_END
