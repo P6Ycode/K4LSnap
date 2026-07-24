0b269df01dd9a9db6dd6f748632f1c4371ee606a
P6Ycode <thalightskintiller@gmail.com>
2026-07-24 12:16:09 -0400

    Add media processing storage paths

--- Headers/K4LSystem.h
+++ Headers/K4LSystem.h
@@ -10,9 +10,12 @@ FOUNDATION_EXPORT NSString * const K4LNotifyDaemonPong;
 
 FOUNDATION_EXPORT NSString *K4LRootDirectory(void);
 FOUNDATION_EXPORT NSString *K4LMediaDirectory(void);
+FOUNDATION_EXPORT NSString *K4LThumbnailDirectory(void);
 FOUNDATION_EXPORT NSString *K4LTemporaryDirectory(void);
+FOUNDATION_EXPORT NSString *K4LDraftDirectory(void);
 FOUNDATION_EXPORT NSString *K4LDatabasePath(void);
 FOUNDATION_EXPORT NSString *K4LPreferencesPath(void);
+FOUNDATION_EXPORT NSString *K4LPendingSendPath(void);
 
 FOUNDATION_EXPORT BOOL K4LEnsureSystemDirectories(NSError **error);
 FOUNDATION_EXPORT void K4LPostDarwinNotification(NSString *name);
