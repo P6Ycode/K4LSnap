afa3781a50c24bf9e274532c4f1b22e7a5d063c9
P6Ycode <thalightskintiller@gmail.com>
2026-07-24 12:21:00 -0400

    Add media editor interface

---
+++ Headers/K4LMediaEditorViewController.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface K4LMediaEditorViewController : UIViewController
+- (instancetype)initWithSourceURL:(NSURL *)sourceURL mediaType:(NSString *)mediaType;
+@end
+
+NS_ASSUME_NONNULL_END
