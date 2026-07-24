dda7bc930fd15ca152b9e2e556819b9d07683865
P6Ycode <thalightskintiller@gmail.com>
2026-07-24 12:22:46 -0400

    Route gallery imports through media editor

--- Sources/K4LGalleryUploadCoordinator.m
+++ Sources/K4LGalleryUploadCoordinator.m
@@ -1,7 +1,7 @@
 #import "K4LGalleryUploadCoordinator.h"
-#import "K4LVaultStore.h"
 #import "K4LFeaturePolicy.h"
 #import "K4LSystem.h"
+#import "K4LMediaEditorViewController.h"
 #import <PhotosUI/PhotosUI.h>
 #import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
 
@@ -10,7 +10,14 @@
 @end
 
 @implementation K4LGalleryUploadCoordinator
-+ (instancetype)sharedCoordinator { static id value; static dispatch_once_t once; dispatch_once(&once, ^{ value = [self new]; }); return value; }
+
++ (instancetype)sharedCoordinator {
+    static id value;
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{ value = [self new]; });
+    return value;
+}
+
 - (void)presentGalleryFromViewController:(UIViewController *)presenter {
     if (![[K4LFeaturePolicy sharedPolicy] galleryUploadEnabled]) return;
     self.presenter = presenter;
@@ -21,6 +28,7 @@
     picker.delegate = self;
     [presenter presentViewController:picker animated:YES completion:nil];
 }
+
 - (void)presentFilesFromViewController:(UIViewController *)presenter {
     if (![[K4LFeaturePolicy sharedPolicy] galleryUploadEnabled]) return;
     self.presenter = presenter;
@@ -29,35 +37,79 @@
     picker.allowsMultipleSelection = NO;
     [presenter presentViewController:picker animated:YES completion:nil];
 }
+
+- (NSString *)mediaTypeForURL:(NSURL *)url preferredTypeIdentifier:(NSString *)typeIdentifier {
+    UTType *type = typeIdentifier.length ? [UTType typeWithIdentifier:typeIdentifier] : [UTType typeWithFilenameExtension:url.pathExtension];
+    return [type conformsToType:UTTypeMovie] ? @"video" : @"image";
+}
+
+- (NSURL *)stageURL:(NSURL *)sourceURL error:(NSError **)error {
+    if (!K4LEnsureSystemDirectories(error)) return nil;
+    NSString *extension = sourceURL.pathExtension.length ? sourceURL.pathExtension.lowercaseString : @"bin";
+    NSURL *destination = [NSURL fileURLWithPath:[K4LTemporaryDirectory() stringByAppendingPathComponent:[NSUUID.UUID.UUIDString stringByAppendingPathExtension:extension]]];
+    [NSFileManager.defaultManager removeItemAtURL:destination error:nil];
+    BOOL accessed = [sourceURL startAccessingSecurityScopedResource];
+    BOOL copied = [NSFileManager.defaultManager copyItemAtURL:sourceURL toURL:destination error:error];
+    if (accessed) [sourceURL stopAccessingSecurityScopedResource];
+    return copied ? destination : nil;
+}
+
 - (void)picker:(PHPickerViewController *)picker didFinishPicking:(NSArray<PHPickerResult *> *)results {
     [picker dismissViewControllerAnimated:YES completion:nil];
     NSItemProvider *provider = results.firstObject.itemProvider;
     if (!provider) return;
     NSString *typeIdentifier = [provider hasItemConformingToTypeIdentifier:UTTypeMovie.identifier] ? UTTypeMovie.identifier : UTTypeImage.identifier;
     [provider loadFileRepresentationForTypeIdentifier:typeIdentifier completionHandler:^(NSURL *url, NSError *error) {
-        if (!url || error) { [self presentError:error ?: [NSError errorWithDomain:@"com.p6ycode.k4lsnap.gallery" code:1 userInfo:@{NSLocalizedDescriptionKey:@"Unable to load selected media"}]]; return; }
-        NSString *extension = url.pathExtension.length ? url.pathExtension : @"bin";
-        NSURL *temporary = [NSURL fileURLWithPath:[K4LTemporaryDirectory() stringByAppendingPathComponent:[NSUUID.UUID.UUIDString stringByAppendingPathExtension:extension]]];
-        NSError *copyError = nil;
-        [NSFileManager.defaultManager removeItemAtURL:temporary error:nil];
-        [NSFileManager.defaultManager copyItemAtURL:url toURL:temporary error:&copyError];
-        if (copyError) { [self presentError:copyError]; return; }
-        [self importAndShareURL:temporary];
+        if (!url || error) {
+            [self presentError:error ?: [NSError errorWithDomain:@"com.p6ycode.k4lsnap.gallery" code:1 userInfo:@{NSLocalizedDescriptionKey: @"Unable to load selected media"}]];
+            return;
+        }
+        NSError *stageError = nil;
+        NSURL *stagedURL = [self stageURL:url error:&stageError];
+        if (!stagedURL) {
+            [self presentError:stageError];
+            return;
+        }
+        [self presentEditorForURL:stagedURL mediaType:[self mediaTypeForURL:url preferredTypeIdentifier:typeIdentifier]];
     }];
 }
-- (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentsAtURLs:(NSArray<NSURL *> *)urls { NSURL *url = urls.firstObject; if (url) [self importAndShareURL:url]; }
-- (void)importAndShareURL:(NSURL *)url {
-    NSError *error = nil;
-    K4LVaultItem *item = [[K4LVaultStore shared] importFileAtURL:url accountID:nil friendID:nil category:@"Gallery Upload" error:&error];
-    if (!item) { [self presentError:error]; return; }
-    NSURL *storedURL = [NSURL fileURLWithPath:[K4LMediaDirectory() stringByAppendingPathComponent:item.relativePath]];
+
+- (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentsAtURLs:(NSArray<NSURL *> *)urls {
+    NSURL *url = urls.firstObject;
+    if (!url) return;
+    NSError *stageError = nil;
+    NSURL *stagedURL = [self stageURL:url error:&stageError];
+    if (!stagedURL) {
+        [self presentError:stageError];
+        return;
+    }
+    [self presentEditorForURL:stagedURL mediaType:[self mediaTypeForURL:url preferredTypeIdentifier:nil]];
+}
+
+- (void)presentEditorForURL:(NSURL *)url mediaType:(NSString *)mediaType {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIViewController *presenter = self.presenter;
+        if (!presenter) {
+            [NSFileManager.defaultManager removeItemAtURL:url error:nil];
+            return;
+        }
+        K4LMediaEditorViewController *editor = [[K4LMediaEditorViewController alloc] initWithSourceURL:url mediaType:mediaType];
+        UINavigationController *navigation = [[UINavigationController alloc] initWithRootViewController:editor];
+        navigation.modalPresentationStyle = UIModalPresentationPageSheet;
+        [presenter presentViewController:navigation animated:YES completion:nil];
+    });
+}
+
+- (void)presentError:(NSError *)error {
     dispatch_async(dispatch_get_main_queue(), ^{
         UIViewController *presenter = self.presenter;
         if (!presenter) return;
-        UIActivityViewController *share = [[UIActivityViewController alloc] initWithActivityItems:@[storedURL] applicationActivities:nil];
-        if (share.popoverPresentationController) { share.popoverPresentationController.sourceView = presenter.view; share.popoverPresentationController.sourceRect = CGRectMake(CGRectGetMidX(presenter.view.bounds), CGRectGetMidY(presenter.view.bounds), 1, 1); }
-        [presenter presentViewController:share animated:YES completion:nil];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"K4LSnap"
+                                                                       message:error.localizedDescription
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        [presenter presentViewController:alert animated:YES completion:nil];
     });
 }
-- (void)presentError:(NSError *)error { dispatch_async(dispatch_get_main_queue(), ^{ UIViewController *presenter=self.presenter; if(!presenter)return; UIAlertController *alert=[UIAlertController alertControllerWithTitle:@"K4LSnap" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert]; [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]]; [presenter presentViewController:alert animated:YES completion:nil]; }); }
+
 @end
