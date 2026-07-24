444d693c5fc4bd51eb62807b95e86095e548be2d
P6Ycode <thalightskintiller@gmail.com>
2026-07-24 11:48:09 -0400

    Add media vault store interface

---
+++ Headers/K4LVaultStore.h
@@ -0,0 +1,32 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface K4LVaultItem : NSObject
+@property (nonatomic, copy) NSString *identifier;
+@property (nonatomic, copy) NSString *relativePath;
+@property (nonatomic, copy, nullable) NSString *accountID;
+@property (nonatomic, copy, nullable) NSString *friendID;
+@property (nonatomic, copy, nullable) NSString *category;
+@property (nonatomic, copy) NSString *mediaType;
+@property (nonatomic) NSTimeInterval createdAt;
+@property (nonatomic) unsigned long long byteSize;
+@end
+
+@interface K4LVaultStore : NSObject
++ (instancetype)shared;
+- (BOOL)open:(NSError **)error;
+- (K4LVaultItem * _Nullable)importFileAtURL:(NSURL *)sourceURL
+                                 accountID:(NSString * _Nullable)accountID
+                                  friendID:(NSString * _Nullable)friendID
+                                  category:(NSString * _Nullable)category
+                                     error:(NSError **)error;
+- (NSArray<K4LVaultItem *> *)itemsForAccount:(NSString * _Nullable)accountID
+                                    friendID:(NSString * _Nullable)friendID
+                                    category:(NSString * _Nullable)category
+                                       error:(NSError **)error;
+- (BOOL)deleteItem:(K4LVaultItem *)item error:(NSError **)error;
+- (NSDictionary<NSString *, NSNumber *> *)categoryCountsForAccount:(NSString * _Nullable)accountID error:(NSError **)error;
+@end
+
+NS_ASSUME_NONNULL_END
